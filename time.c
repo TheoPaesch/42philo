031ede3f052de37c65ea8fbaae0fe8a0d9460ebb
Theo Paesch <t-o@Theos-Air.localdomain>
2024-04-24 17:09:25 +0200

    added time func

---
+++ time.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   time.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tpaesch <tpaesch@student.42heilbronn.de    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/24 16:29:09 by tpaesch           #+#    #+#             */
+/*   Updated: 2024/04/24 16:42:46 by tpaesch          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+unsigned int	ft_get_millis(void)
+{
+	struct timeval	time;
+
+	gettimeofday(&time, NULL);
+	return (time.tv_sec * 1000 + time.tv_usec / 1000);
+}
+
+void	ft_wait_until(unsigned int time)
+{
+	unsigned int	diff_time;
+	unsigned int	current_time;
+
+	while (true)
+	{
+		current_time = ft_get_millis();
+		if (current_time >= time)
+			break ;
+		diff_time = (time - current_time) * 1000;
+		if (diff_time < 5000)
+			usleep(100);
+		else
+			usleep(diff_time * 0.9);
+	}
+}
